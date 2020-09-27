5de52bc68fd477a7ec21e85e1d171d9edb8536e8
Kevin S <alphalop42@gmail.com>
2020-09-27 00:36:10 -0700

    Incremental commit First initial commit General css and html design set up Basic math functions and operate function set up

---
+++ script.js
@@ -0,0 +1,22 @@
+
+//Math functions
+function add(num1, num2){
+    return num1 + num2;
+}
+function subtract(num1, num2){
+    return num1 - num2;
+}
+function multiply(num1, num2){
+    return num1 * num2;
+}
+function divide(num1, num2){
+    return num1 / num2;
+}
+
+//--------
+function operate(operator, num1, num2){
+    if(operator === 'plus'){return add(num1, num2);}
+    else if(operator === 'minus'){return subtract(num1, num2);}
+    else if(operator === 'multiply'){return multiply(num1, num2);}
+    else if(operator === 'divide'){return divide(num1, num2);}
+}
