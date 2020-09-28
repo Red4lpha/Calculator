9b21a389f7849c09365bd55eba4a5484ce4f1b0d
Kevin S <alphalop42@gmail.com>
2020-09-27 21:09:50 -0700

    Incremental Changed CSS to align text to the right Wired up the number buttons to display to screen Wured up C button to clear out display

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+const numKeys = document.querySelectorAll('.btn');
+const display = document.querySelector('.display')
+let displayText = '';
+numKeys.forEach((numKey) => {
+    numKey.addEventListener('click', click);
+  });
+function click(e){
+    console.log(e.target.innerText);
+    //console.log(e.target.name);
+    
+    //number pressed
+    if(e.target.className === 'btn num'){
+       displayText += e.target.innerText;
+       display.textContent =  displayText;    
+    }
+    //clear pressed
+    else if(e.target.name === 'clear'){
+        displayText = '';
+        display.textContent =  displayText; 
+    }
+}  
+
+
+
+
+
 
 //Math functions
 function add(num1, num2){
