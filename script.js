5019cf79b88741b75f6770796d09256bea9a8474
Kevin S <alphalop42@gmail.com>
2020-09-29 21:57:34 -0700

    Incremental Wired up equals > Added functionality to keep on tacking on operators without hitting equals > Added some basic error handling > Wired up the +/- button

--- script.js
+++ script.js
@@ -1,47 +1,108 @@
 const numKeys = document.querySelectorAll('.btn');
 const displayBot = document.querySelector('#displayBot')
 let displayText = '';
-let operandOne = 0;
-let operandTwo = 0; 
+let operandOne = '';
+let operandTwo = ''; 
 let operator = '';
+let lastPress = '';
 numKeys.forEach((numKey) => {
     numKey.addEventListener('click', click);
   });
 function click(e){
     console.log(e.target.innerText);
     //console.log(e.target.name);
-    
-    //number pressed
+    //number pressed     
     if(e.target.className === 'btn num'){
-       displayText += e.target.innerText;
-       displayBot.textContent =  displayText;    
+        if(lastPress === 'equal'){
+            displayText = '';
+            lastPress = '';
+        }
+        displayText += e.target.innerText;
+        displayBot.textContent =  displayText;
+        displayConsoleInfo();    
     }
     //clear pressed
     else if(e.target.name === 'clear'){
         clear();
+        displayConsoleInfo();  
     }
     else if(e.target.className === 'btn operator'){
-        if(operandOne === 0){
+        //if this is the first command and no numbers are present
+        if(displayText === ''){
+            displayConsoleInfo(); 
+            return;
+             
+        }
+        else if(e.target.name === 'sign'){
+            if(displayText > 0){
+                //let temp = '-';
+                displayText = '-' + displayText;
+                displayBot.textContent =  displayText;
+                displayConsoleInfo(); 
+                return;
+            }
+            else if(displayText < 0) {
+                //let temp = displayText.replace('-','');
+                let temp = Math.abs(displayText);
+                displayText = temp;
+                displayBot.textContent =  displayText;
+                displayConsoleInfo(); 
+                return;
+            }
+        }
+        
+        //if this is a first operator called after a series of numbers have been displayed
+        else if(operandOne === ''){
+            console.log("btnOp - operandOne == Null called");
             operandOne = displayText;
-            console.log(e.target.name);
             operator = e.target.name;
             displayText = '';
+            displayConsoleInfo();  
+        }
+        // 
+        else if(operandOne != ''){
+            console.log("btnOp - operandOne == # called");
+            equal();
+            lastPress = 'equal';
+            operator = e.target.name;
+            operandOne = displayText;
+            displayConsoleInfo();  
         }
     }
     else if(e.target.className === 'btn equal'){
-        if(operandOne === 0){
-            return;
+        if (displayText === ''){return;}
+        else if(operandOne === ''){return;}
+        else{
+            equal();
+            lastPress = e.target.name;
+            displayConsoleInfo();  
         }
-        operandTwo = displayText;
-        displayText = operate(operator, operandOne, operandTwo);
-        displayBot.textContent =  displayText; 
     }
-}  
+}
+function displayConsoleInfo(e){
+    console.log("OperandOne = " + operandOne);    
+    console.log("OperandTwo = " + operandTwo);
+    console.log("Operator = " + operator);
+    console.log("DisplayText = " + displayText);
+    console.log("lastPress = " + lastPress); 
+}
+
+function equal(e){
+    operandTwo = displayText;
+    displayText = operate(operator, operandOne, operandTwo);
+    displayBot.textContent =  displayText; 
+    operandOne = operandTwo = operator =  '';
+}
+
+
+
+
 //Special functions
-    function clear(){
-        displayText = '';
-        displayBot.textContent =  displayText; 
-    }
+function clear(){
+    displayText = '';
+    operator = '';
+    displayBot.textContent =  displayText; 
+}
 
 
 
@@ -49,7 +110,7 @@ function click(e){
 
 //Math functions
 function add(num1, num2){
-    return num1 + num2;
+    return parseFloat(num1) + parseFloat(num2);
 }
 function subtract(num1, num2){
     return num1 - num2;
